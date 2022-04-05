23d644a44c178b5c8e4e9f1ef441d82c9c9c10df
Henrik Sjööh <henriiik@gmail.com>
2022-04-05 06:42:11 +0200

    add issue reproduction

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as awsnative from "@pulumi/aws-native";
+
+const webAcl = new awsnative.wafv2.WebACL(`web-acl`, {
+	defaultAction: { allow: {} },
+	scope: "REGIONAL",
+	visibilityConfig: {
+		// trigger an update every time
+		metricName: `${Date.now()}`,
+		cloudWatchMetricsEnabled: false,
+		sampledRequestsEnabled: false
+	},
+	tags: [
+		{
+			key: "key",
+			value: "val"
+		}
+	]
+});
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Unable to update awsnative.wafv2.WebAcl without updating tags
+
+This is a minimal reproduction for the the issue in the `@pulumi/aws-native` repo.
+
+https://github.com/pulumi/pulumi-aws-native/issues/415
